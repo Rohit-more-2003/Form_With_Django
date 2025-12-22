d9a03b8af470e895603bc2937a56689dc70570cf
Rohit_More <morer4851@gmail.com>
2025-12-22 22:05:37 +0530

    Implemented 'views' and 'forms' files to obtain the user input data

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 # Create your views here.
 def index(request):
+	if request.method == "POST":
+		form = ApplicationForm(request.POST)
+		if form.is_valid():
+			first_name = form.cleaned_data['first_name']
+			last_name = form.cleaned_data['last_name']
+			email = form.cleaned_data['email']
+			date = form.cleaned_data['date']
+			occupation = form.cleaned_data['occupation']
+			
+			# This is to confirm whether we got the user input data or not
+			# print(first_name)
+		
 	return render(request, "index.html")
